bb9a7981085d7b1e4ad488c5b3fddc0563e6875a
Ivan chiari <Ivan31@mhs.unsyiah.ac.id>
2023-10-23 23:03:41 +0700

    design basic database schema

---
+++ culina-base/database/migrations/2023_10_23_155439_create_recipe_ingredient_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('recipe_ingredient', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('recipe_id');
+            $table->unsignedBigInteger('ingredient_id');
+            $table->decimal('quantity');
+            $table->string('unit');
+            $table->timestamps();
+
+            $table->foreign('recipe_id')->references('id')->on('recipes');
+            $table->foreign('ingredient_id')->references('id')->on('ingredients');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipe_ingredient');
+    }
+};
