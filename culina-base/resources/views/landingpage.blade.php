5dc5aea621a2123466905f20afdffdc73a9482e5
Ivan chiari <Ivan31@mhs.unsyiah.ac.id>
2023-10-25 13:58:49 +0700

    modified some page

--- culina-base/resources/views/landingpage.blade.php
+++ culina-base/resources/views/landingpage.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,16 +8,17 @@
     <title>CulinaBase</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
+
     <div class="container">
-        
+
         <header>
             <div class="navContainer">
                 <nav>
                     <ul id="navList">
                         <li>
-                            <p id="navLogo" href="/">CulinaBase</p>
+                            <a id="navLogo" href="/">CulinaBase</a>
                         </li>
                         <li>
                             <div id="navItems">
@@ -31,18 +33,136 @@
         </header>
 
         <main>
-            <p>test</p>
+
+            <div class="welcome">
+                <h1>Welcome</h1>
+                <p>Discover a world of delicious recipes for your favorite dishes and drinks.</p>
+            </div>
+
+            <div class="recipe-cards">
+                <div class="recipe-card">
+                    <a href="#">
+                        <img src="recipe/carbonara.jpg" alt="Spaghetti Carbonara">
+                        <h2>Spaghetti Carbonara</h2>
+                        <p>Masakan pasta klasik Italia dengan saus telur, keju parmesan, dan daging bacon yang lezat.
+                        </p>
+                        <a class="read-more" href="#">Lanjutkan Membaca</a>
+                    </a>
+                </div>
+
+                <div class="recipe-card">
+                    <a href="#">
+                        <img src="recipe/Chicken_Alfredo.jpg" alt="Chicken Alfredo">
+                        <h2>Chicken Alfredo</h2>
+                        <p>Hidangan pasta dengan saus krim yang lezat, disertai potongan ayam yang gurih, dan pasta yang
+                            lembut.</p>
+                        <a class="read-more" href="#">Lanjutkan Membaca</a>
+                    </a>
+                </div>
+
+                <div class="recipe-card">
+                    <a href="#">
+                        <img src="recipe/Margherita_Pizza.jpg" alt="Margherita Pizza">
+                        <h2>Margherita Pizza</h2>
+                        <p>Pizza klasik Italia dengan saus tomat, mozzarella segar, dan daun basil yang harum.</p>
+                        <a class="read-more" href="#">Lanjutkan Membaca</a>
+                    </a>
+                </div>
+
+                <div class="recipe-card">
+                    <a href="#">
+                        <img src="recipe/Beef-Stir-Fry.jpg" alt="Beef Stir-Fry">
+                        <h2>Beef Stir-Fry</h2>
+                        <p>Hidangan daging sapi yang dimasak cepat dengan sayuran dan saus gurih, ideal untuk makan
+                            malam cepat.</p>
+                        <a class="read-more" href="#">Lanjutkan Membaca</a>
+                    </a>
+                </div>
+
+                <div class="recipe-card">
+                    <a href="#">
+                        <img src="recipe/Caesar-Salad.jpg" alt="Caesar Salad">
+                        <h2>Caesar Salad</h2>
+                        <p>Salad segar dengan daun romaine, crouton, parmesan, dan saus Caesar yang khas.</p>
+                        <a class="read-more" href="#">Lanjutkan Membaca</a>
+                    </a>
+                </div>
+
+                <div class="recipe-card">
+                    <a href="#">
+                        <img src="recipe/Grilled_Salmon.jpeg" alt="Grilled Salmon">
+                        <h2>Grilled Salmon</h2>
+                        <p>Salmon panggang yang lezat, disajikan dengan bumbu dan saus khusus.</p>
+                        <a class="read-more" href="#">Lanjutkan Membaca</a>
+                    </a>
+                </div>
+
+                <div class="recipe-card">
+                    <a href="#">
+                        <img src="recipe/Vegetable_Curry.jpg" alt="Vegetable Curry">
+                        <h2>Vegetable Curry</h2>
+                        <p>Semangkuk kari lezat dengan sayuran yang dimasak dalam saus rempah-rempah.</p>
+                        <a class="read-more" href="#">Lanjutkan Membaca</a>
+                    </a>
+                </div>
+
+                <div class="recipe-card">
+                    <a href="#">
+                        <img src="recipe/Mango_Smoothie.jpeg" alt="Mango Smoothie">
+                        <h2>Mango Smoothie</h2>
+                        <p>Minuman segar yang terbuat dari mangga matang, yogurt, dan es, sempurna untuk cuaca panas.
+                        </p>
+                        <a class="read-more" href="#">Lanjutkan Membaca</a>
+                    </a>
+                </div>
+
+                <div class="recipe-card">
+                    <a href="#">
+                        <img src="recipe/Chocolate_Brownies.jpeg" alt="Chocolate Brownies">
+                        <h2>Chocolate Brownies</h2>
+                        <p>Brownies cokelat yang lembut dan renyah, cocok untuk pencinta cokelat.</p>
+                        <a class="read-more" href="#">Lanjutkan Membaca</a>
+                    </a>
+                </div>
+
+                <!-- Add more recipe cards as needed -->
+
+            </div>
+
+
+            <section class="about-us-box">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="about-us-content text-center" style="background-image: url(gambar/bg4.jpg);">
+                                <h2>About Us</h2>
+                                <p>Website ini merupakan kumpulan resep masakan dari berbagai sumber. Kami menyediakan
+                                    berbagai
+                                    resep masakan,</p>
+                                <p>mulai dari masakan Indonesia, masakan internasional, hingga masakan
+                                    vegetarian.</p>
+                                <p>Tujuan kami adalah untuk memudahkan para pecinta kuliner menemukan resep yang mereka
+                                    cari. </p>
+                                <p>Kami juga ingin berbagi informasi dan tips memasak kepada para pembaca.</p>
+                                <a class="read-more" href="/about">Learn more about our team</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
         </main>
 
         <footer>
-           <div class="footerBox">
+            <div class="footerBox">
                 <p>
-                    Copyright  &copy;CulinaBase kelompok Sistem Informasi 
-           </div>
+                    Copyright &copy; CulinaBase kelompok Sistem Informasi
+                </p>
+            </div>
         </footer>
-
 
     </div>
 
 </body>
+
 </html>
